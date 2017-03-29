442d1cec9264154d1de96fe5605f3392797cb597
Jennifer Oakes <joakes10@gmail.com>
2017-03-29 16:51:31 -0400

    Simplified file and moved functionality to service file

--- src/js/inventory.controller.js
+++ src/js/inventory.controller.js
@@ -4,28 +4,15 @@
   angular.module('inventory') //retreiving a module
   .controller('InventoryController', InventoryController);
 
-  function InventoryController() {
+  InventoryController.$inject = ['InventoryService'];
+
+  function InventoryController(InventoryService) {
 
     let vm = this;
-
     vm.newItem = {};
     vm.sortType = 'price'; // set the default sort type
     vm.sortReverse = false; // set the default sort order
-
-    vm.inventory = [
-      { "id": 2957, "name": "widget", "price": 32, "quantity": 203, "color": "red", "discount": 31 },
-      { "id": 89274, "name": "golf club", "price": 98, "quantity": 10, "color": "black", "discount": 0 },
-      { "id": 64, "name": "iPhone", "price": 499, "quantity": 2, "color": "white", "discount": 0 },
-      { "id": 87363, "name": "bonzai tree", "price": 76, "quantity": 2, "color": "green", "discount": 0 },
-      { "id": 1736, "name": "towel", "price": 55, "quantity": 30, "color": "brown", "discount": 10 },
-      { "id": 4836, "name": "dog bed", "price": 99, "quantity": 10, "color": "beige", "discount": 50 },
-      { "id": 829, "name": "waste basket", "price": 15, "quantity": 40, "color": "silver", "discount": 0 },
-      { "id": 46, "name": "guitar", "price": 899, "quantity": 0, "color": "blue", "discount": 150 },
-      { "id": 17456, "name": "matcha tea", "price": 42, "quantity": 4, "color": "green", "discount": 11 },
-      { "id": 3292, "name": "enlightenment", "price": 99999, "quantity": 1, "color": "red", "discount": 0 },
-      { "id": 533, "name": "eggs", "price": 5, "quantity": 12, "color": "brown", "discount": 1 },
-      { "id": 683, "name": "pillow", "price": 27, "quantity": 10, "color": "black", "discount": 12 }
-    ];
+    vm.inventory = InventoryService.getAllItems();
 
     /**
     * Calculates price of inventory item, taking into consideration discount and tax
@@ -44,34 +31,10 @@
      * @return {void}
      */
     vm.addItem = function addItem(item) {
-      if(typeof(item) !== 'object') {
-        return;
-      }
-
-      if(typeof(item.name) !== 'string' || item.name.length < 1) {
-        return;
-      }
-
-      item.price = Number(item.price); //setting price to be a number since all data coming through a form is a string
-      if( Number.isNaN(item.price)) {
-        return;
-      }
-
-      let id = Date.now();
-      //gives us the number of milliseconds from January 1st, 1970
-      //this 'fakes' a random number
-
-      vm.inventory.push({
-        id: id,
-        name: item.name,
-        price: item.price,
-        quantity: item.quantity,
-        color: item.color,
-        discount: item.discount
-      });
+      InventoryService.addItemToInventory(item);
       vm.newItem = {};
-      //this clears the input fields after you submit a 'new item'
     };
+
     vm.changeSort = function changeSort(sortField) {
       vm.sortType = sortField;
       vm.sortReverse = !vm.sortReverse;
