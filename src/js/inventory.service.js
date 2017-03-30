d13a503dd8d70e7d7545f9bff78d06efe691dd7a
Jennifer Oakes <joakes10@gmail.com>
2017-03-30 14:12:16 -0400

    Added tests to make sure quantity and price were greater than or equal to zero

--- src/js/inventory.service.js
+++ src/js/inventory.service.js
@@ -1,9 +1,10 @@
 (function() {
   'use strict';
 
-  angular.module('inventory').factory('InventoryService', InventoryService);
+  angular.module('inventory') //retreiving the inventory module
+  .factory('InventoryService', InventoryService); //creating a factory
 
-  function InventoryService() {
+  function InventoryService() { //constructor function
 
     let items = JSON.parse(localStorage.getItem('items')) || [];
 
@@ -22,15 +23,26 @@
       }
 
       item.price = Number(item.price); //setting price to be a number since all data coming through a form is a string
-      if( Number.isNaN(item.price)) {
+      if( Number.isNaN(item.price) || item.price < 0.01) {
         return;
       }
+
+      if(typeof(item.discount) !== 'number' || item.discount >= 0) {
+        return;
+      }
+
+      if(typeof(item.quantity) !== 'number' || item.quantity >= 0) {
+        return;
+      }
+
+      //color? must be a string...if not, do something, use 'null' item.color = null, if not a string, make it null
+      //does that string match a previously defined color?
 
       let id = Date.now();
       //gives us the number of milliseconds from January 1st, 1970
       //this 'fakes' a random number
 
-      items.push({
+      items.push({ //'items' matches line 47 toJson(items)
         id: id,
         name: item.name,
         price: item.price,
@@ -42,6 +54,10 @@
       localStorage.setItem('items', angular.toJson(items));
     }
 
+    /**
+     * gets all the items in inventory
+     * @return {Array}      All items in inventory
+     */
     function getAllItems() {
       return items;
     }
