2f43f0a717e189ac9f240b6227e2027f6a0063f5
Jennifer Oakes <joakes10@gmail.com>
2017-03-30 17:11:27 -0400

    Added one passing test

--- tests/specs/inventory.controller.spec.js
+++ tests/specs/inventory.controller.spec.js
@@ -1,6 +1,20 @@
 (function() {
   'use strict';
 
+  let expect = chai.expect;
+
+  describe('inventory controller', function() {
+
+    let InventoryController;
+    let mockInventoryService = {};
+
+    beforeEach(module('shop'));
+
+    beforeEach(module(function($provide) { //creating a submodule, a module inside of a module
+      $provide.value('InventoryService', mockInventoryService);
+      //the value of the real inventory service is dead, so instead use this fake one
+    }));
+  });
 
 
 }());
--- tests/specs/inventory.service.spec.js
+++ tests/specs/inventory.service.spec.js
@@ -1,6 +1,28 @@
 (function() {
   'use strict';
 
+  let expect = chai.expect;
+
+  describe('inventory service', function() {
+
+    let InventoryService;
+    beforeEach(module('shop')); //you telling angular-mocks to create an ng-app
+
+    beforeEach(inject(function(_InventoryService_) {
+      InventoryService = _InventoryService_;
+    }));
+
+    it('should be able to retreive an array of all the items', function () {
+        let result = InventoryService.getAllItems();
+        expect(result).to.be.an('array');
+    });
+
+    
+
+
+
+
+  });
 
 
 }());
