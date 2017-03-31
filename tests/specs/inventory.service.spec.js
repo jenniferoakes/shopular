8a42a921f8ec538f3193c786bec324f2c3b879d6
Jennifer Oakes <joakes10@gmail.com>
2017-03-31 14:18:24 -0400

    Added test to make sure item was not added to inventory if the price was less than 0.

--- tests/specs/inventory.service.spec.js
+++ tests/specs/inventory.service.spec.js
@@ -4,13 +4,16 @@
   let expect = chai.expect;
 
   describe('inventory service', function() {
-
     let InventoryService;
     beforeEach(module('shop')); //you telling angular-mocks to create an ng-app
 
     beforeEach(inject(function(_InventoryService_) {
       InventoryService = _InventoryService_;
     }));
+
+    afterEach(function() {
+      localStorage.removeItem('items');
+    });
 
     it('should be able to retreive an array of all the items', function () {
         let result = InventoryService.getAllItems();
@@ -35,7 +38,21 @@
       expect(items[0].price).to.equal(7.99);
       expect(items[0].quantity).to.equal(500);
       expect(items[0].discount).to.equal(0.99);
-      expect(items[0].id).to.equal(now);
+    });
+
+    it('should handle if the price is less than 0', function() {
+      expect(InventoryService.getAllItems().length).to.equal(0);
+      let now = Date.now();
+      InventoryService.addItemToInventory({
+        id: now,
+        name: 'Mugs',
+        price: -1,
+        quantity: 500,
+        color: 'Blue',
+        discount: 0.99
+      });
+      let items = InventoryService.getAllItems();
+      expect(items.length).to.equal(0);
     });
 
   });
