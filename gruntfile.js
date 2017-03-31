a94b5cd11cc2b6829332b759a39f878d690c6eed
Jennifer Oakes <joakes10@gmail.com>
2017-03-31 11:44:56 -0400

    Added concat options and which files to concat

--- gruntfile.js
+++ gruntfile.js
@@ -4,6 +4,30 @@ module.exports = function gruntConfig(grunt) {
 
   grunt.initConfig({
 
+    concat: {
+      alljs: {
+        options: {
+          sourceMap: true
+        },
+        src: ['src/js/shop.module.js', 'src/js/**/*.js'],
+        dest: 'build/js/main.js'
+      }
+    },
+
+    babel: {
+      all: { //all js files
+        options: {
+          presets: ['es2015'],
+          sourceMap: true //need this so that when we run the build, the sourcemap will show us where we have errors
+        },
+        files: {
+          'build/js/main.js': 'build/js/main.js'
+          //where we want the files to go in build : where the files are currently
+        }
+      }
+
+    },
+
     karma: { //task name - do not make this up, this is defined already
       all: { // target name - you make this up
         options: {
@@ -16,14 +40,26 @@ module.exports = function gruntConfig(grunt) {
             'src/js/shop.module.js',
             'src/js/**/*.js', //this will not dupilcate the module file
             'tests/**/*.js'
-          ]
+          ],
+          //need to add preprocessor to our tests/code to send it through the coverage tool
+          preprocessors: {
+            'src/js/**/*.js': ['coverage']
+          },
+          //we need to tell it to show us the data
+          reporters: ['dots', 'coverage'],
+          //show test reports as dots, and the coverage reports
+          coverageReporter: {
+            type: 'text-summary'
+          }
         }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('build', [ 'karma' ]);
+  grunt.registerTask('build', [ 'karma', 'concat', 'babel' ]);
 
 };
